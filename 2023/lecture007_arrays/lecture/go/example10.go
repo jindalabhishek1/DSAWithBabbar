26e6e36d180ddc84457024dbd9d0cbcbfb97be5c
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2023-06-12 23:38:06 +0530

    slice

--- 2023/lecture007_arrays/lecture/go/example10.go
+++ 2023/lecture007_arrays/lecture/go/example10.go
@@ -16,12 +16,31 @@ func main() {
 
 	for i := 0; i < arraySize; i++ {
 		fmt.Scan(&array[i])
-		fmt.Printf("Type of array[i]: %T\tType of 0: %T\tType of 1: %T\n", array[i], 0, 1)
+		// fmt.Printf("Type of array[i]: %T\tType of 0: %T\tType of 1: %T\n", array[i], 0, 1)
 		// have a look at the condition
-		if array[i] != 0 || array[i] != 1 {
+		if array[i] != 0 && array[i] != 1 {
 			i--
 			fmt.Println("Valid values are 0 or 1, please enter again")
 		}
 	}
 	fmt.Println(array)
+
+	sortArray(&array, arraySize)
+	fmt.Println(array)
+}
+
+func sortArray(array *[]int, size int) {
+
+	start := 0
+	end := size - 1
+	for i := 0; i < size-1; {
+		if (*array)[i] == 0 {
+			(*array)[i], (*array)[start] = (*array)[start], (*array)[i]
+			start++
+			i++
+		} else {
+			(*array)[i], (*array)[end] = (*array)[end], (*array)[i]
+			end--
+		}
+	}
 }
